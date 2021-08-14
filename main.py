576d168569aecfd93ae63d54d4bb1a1ce711c463
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2021-08-15 03:45:12 +0530

    create a new Pixela graph using API

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from pixela import Pixela
 
 # make an API call to create a new user
 pixela = Pixela()
-print(pixela.create_user_account())
+# print(pixela.create_user_account())
+
+# make a new call to create graph
+print(pixela.create_graph("graph1", "Gym Workout"))
--- pixela.py
+++ pixela.py
@@ -20,3 +20,22 @@ class Pixela:
             json=parameters
         )
         return response.text
+
+    def create_graph(self, graph_id, graph_name):
+        graph_endpoint = f"{self.endpoint}/{self.username}/graphs"
+        headers = {
+            "X-USER-TOKEN": self.token
+        }
+        parameters = {
+            "id": graph_id,
+            "name": graph_name,
+            "unit": "hours",
+            "type": "float",
+            "color": "ajisai",
+        }
+        response = requests.post(
+            url=graph_endpoint,
+            json=parameters,
+            headers=headers
+        )
+        return response.text
